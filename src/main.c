6974e4319e45a254f2dedffabfb22e715e9a1836
Nuga <nuga_agun@proton.me>
2025-11-02 18:46:36 +0100

    Interruption utilisateur

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include "../include/chip8.h"
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+
+char getch(void) {
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcgetattr()");
+    old.c_lflag &= ~ICANON; // Disable line buffering
+    old.c_lflag &= ~ECHO;   // Disable echo
+    old.c_cc[VMIN] = 1;     // Wait for at least one character
+    old.c_cc[VTIME] = 0;    // No timeout
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= ICANON;  // Restore line buffering
+    old.c_lflag |= ECHO;    // Restore echo
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    return buf;
+}
 
 int main (int argc, char* argv[]) {
 	if (argc != 2) {
@@ -25,6 +47,9 @@ int main (int argc, char* argv[]) {
 		//fetch
 		//decode
 		//execute
-		return_code = 1;
+		if (getch() == 27) {
+			printf("Keyboard Interruption");
+			return_code = 1;
+		}
 	} while (return_code == 0);
 }
